8e26c1c11836e55a5bda4b2204d665208c9a90b2
Matija Kukic <kukicmatija@gmail.com>
2023-08-09 18:22:18 +0200

    Dodana nova klasa fruit koja nasljedjuje food.

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ class Food:
         self.article = article
     def __str__(self):
         return f"Food is type: {self.Type}, article {self.article}"
+
+class Fruit(Food):
+    def __init__(self,article,sugar_per_article):
+        super().__init__("Fruit",article)
+        self.sugar_per_article = sugar_per_article
+
+    def __str__(self):
+        return f"This fruit is a/an {self.article} and has {self.sugar_per_article}g of sugar."
+        
+
 def main():
     f1 = Food("Fruit","Banana")
     f2 = Food("Meat","Chicken")
-    print(f1,f2,sep="\n")
+    f3 = Fruit("Apple", 35)
+    print(f1,f2,f3,sep="\n")
+
 
 
 main()
