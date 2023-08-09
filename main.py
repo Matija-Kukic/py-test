2683c57c6561650768377c34b704bd02cb351d23
Matija Kukic <kukicmatija@gmail.com>
2023-08-09 15:23:54 +0200

    Dodana funkcionalnost u main.py, klasa Food i dva primjera instanciranja.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+class Food:
+    def __init__(self,Type,article):
+        self.Type = Type
+        self.article = article
+    def __str__(self):
+        return f"Food is type: {self.Type}, article {self.article}"
+def main():
+    f1 = Food("Fruit","Banana")
+    f2 = Food("Meat","Chicken")
+    print(f1,f2,sep="\n")
+
+
+main()
